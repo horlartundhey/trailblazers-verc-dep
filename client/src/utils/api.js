c0eef0f560f91bcd6008c224b3cea964bea7c096
horlartundhey <horlartundhey@gmail.com>
2025-05-06 12:04:33 +0100

    pushing trailblazer to live11

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create axios instance with base URL
 const API = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://trailblazers-verc-server.vercel.app',
   headers: {
     'Content-Type': 'application/json',
   },
