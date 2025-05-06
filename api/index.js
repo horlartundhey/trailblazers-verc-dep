89661f343a424dba486bcb742ca1b2666b65c492
horlartundhey <horlartundhey@gmail.com>
2025-05-06 11:19:07 +0100

    pushing trailblazer to live6

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
-const app = require('../server/index');
+const index = require('../server/index');
 const serverless = require('serverless-http');
 
-module.exports = serverless(app);
+module.exports = serverless(index);
