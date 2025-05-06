da75dfa26eb10858fc326aa6ee110b84e2733cf7
horlartundhey <horlartundhey@gmail.com>
2025-05-06 11:25:32 +0100

    pushing trailblazer to live7

--- server/index.js
+++ server/index.js
@@ -65,19 +65,24 @@ app.get('/', (req, res) => {
 //   });
 // });
 // Error handling middleware
+// app.use((err, req, res, next) => {
+//   console.error(err.stack);
+//   res.status(500).json({
+//     success: false,
+//     message: 'Server Error',
+//     error: process.env.NODE_ENV === 'development' ? err.message : undefined
+//   });
+// });
+
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).json({
-    success: false,
-    message: 'Server Error',
-    error: process.env.NODE_ENV === 'development' ? err.message : undefined
-  });
+  res.status(500).json({ message: 'Something went wrong!' });
 });
 
 // Start server
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
-module.exports = app;
+// module.exports = app;
